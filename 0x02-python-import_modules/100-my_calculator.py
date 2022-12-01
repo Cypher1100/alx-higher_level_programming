23c053dc770aacc1e9d8715a964d889ccfbb1d8b
Richard Ben-Aboje <richardcyph17@gmail.com>
2022-12-01 15:55:39 -0800

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -9,8 +9,6 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-"""create a dictionary to hold operation balues"""
-
     calc = {"+": add, "-": sub, "*": mul, "/": div}
     if sys.argv[2] not in list(calc.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
