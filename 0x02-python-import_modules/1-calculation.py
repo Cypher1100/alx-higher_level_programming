94168fdf31b099c1f3c68c73e5cebd7979476a20
Richard Ben-Aboje <richardcyph17@gmail.com>
2022-12-01 13:09:13 -0800

    1-calculation.py

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+a = 1
+b = 5
+
+print("{:d} + {:d} = {:d}".format(a, b, calc.add(a, b)))
+
+print("{:d} + {:d} = {:d}".format(a, b, calc.sub(a, b)))
+
+print("{:d} + {:d} = {:d}".format(a, b, calc.mul(a, b)))
+
+print("{:d} + {:d} = {:d}".format(a, b, calc.div(a, b)))
+
