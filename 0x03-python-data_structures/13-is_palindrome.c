3d2a4014e40fb9b86be509f6c6f214657d455e88
Richard Ben-Aboje <richardcyph17@gmail.com>
2022-12-05 14:03:25 -0800

    task 13

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,63 @@
+#include "lists.h"
+
+/**
+ * reverse - reverses the other side of the list *
+ * @h_r: head of the other side of the lise
+ * Return: nothing
+ */
+void reverse(listint_t **h_r)
+{
+	listint_t *prv;
+	listint_t *crr;
+	listint_t *nxt;
+
+	prv = NULL;
+	crr = *h_r;
+
+	while (crr != NULL)
+	{
+		nxt = crr->next;
+		crr->next = prv;
+		prv = crr;
+		crr = nxt;
+	}
+
+	*h_r = prv;
+}
+
+/**
+ * compare - compares each integer of the list
+ *
+ * @h1: head of the first side
+ * @h2: head of the second side
+ * Return: 1 if they are equal, 
+ * and 0 if not equal
+ */
+int compare(listint_t *h1, listint_t *h2)
+{
+	listint_t *tmp1;
+	listint_t *tmp2;
+
+	tmp1 = h1;
+	tmp2 = h2;
+
+	while (tmp1 != NULL && tmp2 != NULL)
+	{
+		if (tmp1->n == tmp2->n)
+		{
+			tmp1 = tmp1->next;
+			tmp2 = tmp2->next;
+		}
+		else
+		{
+			return (0);
+		}
+	}
+
+	if (tmp1 == NULL && tmp2 == NULL)
+	{
+		return (1);
+	}
+
+	return (0);
+}
