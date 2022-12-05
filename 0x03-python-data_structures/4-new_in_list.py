b6bb7bce9cf36d3be6b96c81e87abf10d2e59435
Richard Ben-Aboje <richardcyph17@gmail.com>
2022-12-05 09:49:15 -0800

    task 4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+       new_list = my_list[:]
+       listlength = len(my_list)
+
+       if idx >= 0 and idx < listlength:
+           new_list[idx] = element
+
+           return (new_list)
