8e8f3d807081d8d47fef7724b77a9ab4d3e0d22d
Richard Ben-Aboje <richardcyph17@gmail.com>
2022-12-07 00:28:29 -0800

    1-search_replace.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+    square_matrix = matrix.copy()
+    
     for num in range(len(matrix)):
-        squarematrix = list(map(lambda num: num*num,matrix[num]))
-    return (squarematrix)
+        square_matrix[num] = list(map(lambda x: x*x, matrix[num]))
+    return (square_matrix)
---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    temp_list = list(map(lambda num: replace if num == search else num, my_list))
+    return temp_list
