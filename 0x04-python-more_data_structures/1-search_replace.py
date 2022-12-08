5c0398b82b51ff418067c763aab872a5492ea834
Richard Ben-Aboje <richardcyph17@gmail.com>
2022-12-08 09:25:05 -0800

    tas 0&1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    temp_list = list(map(lambda num:
-        replace if num == search else num, my_list))
+    temp_list = list(map(lambda num: replace if num == search else num, my_list))
     return temp_list
