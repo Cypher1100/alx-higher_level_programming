eaf57c6a4c4bacf4014b1a10eb8f1dc79364ebc3
Richard Ben-Aboje <richardcyph17@gmail.com>
2022-12-08 07:57:22 -0800

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,6 +2,10 @@
 def roman_to_int(roman_string):
     rd = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     result = 0
+    if not roman_string:
+        return 0
+    if not isinstance(roman_string):
+        return o
 
     for rs in range(len(roman_string)):
         if rs + 1 != len(roman_string) and rd[roman_sring[rs]] < rd[roman_string[rs + 1]]:
