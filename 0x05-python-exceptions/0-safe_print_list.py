b2fb771aac900885d46e104b47142d19b7f70d61
Richard Ben-Aboje <richardcyph17@gmail.com>
2022-12-19 09:18:49 -0800

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    totalent = 0
+    lentcounter = 0
     for element in range(x):
         try:
             print(f"{my_list[element]}", end="")
-            totalen = totalen + 1
+            lentcounter = lentcounter + 1
         except IndexError:
             break
     print()
-    return(totalen)
+    return(lentcounter)
