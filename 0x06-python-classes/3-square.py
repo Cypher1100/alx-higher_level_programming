2a179b7052359084c6a95f76a60bd3b56a2b71b0
Richard Ben-Aboje <richardcyph17@gmail.com>
2022-12-20 10:48:00 -0800

    3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python3
-"""Defines a square"""
+"""Defines a square """
 
 
 class Square:
+    """Represents a square object"""
+
     def __init__(self, size=0):
+
         if not isinstance(size, int):
-            raise TypeError("size must be an integer")
+            raise TypeError('size must be an integer')
         if size < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError('size must be >= 0')
+
         self.__size = size
 
-
     def area(self):
+        """
+        Methot calculates the area of the square
+        Returns: The square of the size
+        """
+
         return (self.__size ** 2)
