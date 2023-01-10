1ee877cfc40989ecb6f334212afbc0cd674dda4a
Richard Ben-Aboje <richardcyph17@gmail.com>
2023-01-10 13:16:47 -0800

    task 10 to 12

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""This module defines a Student class"""
+
+
+class Student:
+    """Represent a student objeft."""
+
+    def __init__(self, first_name, last_name, age):
+        """Instatiattion of a new Student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Function retrieves a dictionary representation of the Student
+        If attrs is a list of strings, retrieves only those attributes
+        included in the list
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
