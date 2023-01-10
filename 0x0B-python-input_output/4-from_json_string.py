8177c7771bfec0cdb4770f53c95568414f7363ac
Richard Ben-Aboje <richardcyph17@gmail.com>
2023-01-10 11:09:17 -0800

    Task 4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module JSON-to-object function"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """This function returns the Python object 
+    representation of a JSON string
+    """
+    return json.loads(my_str)
