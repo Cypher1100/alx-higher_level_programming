75c1f4bf4ee9456b78a722d44ee66f3666a17d04
Richard Ben-Aboje <richardcyph17@gmail.com>
2023-01-10 10:35:04 -0800

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-"""This module defines a function that reads a text file (UTF8) and prints it to stdout"""
+"""
+This module defines a function that reads a text file (UTF8) 
+and prints it to stdout
+"""
 
 
 def read_file(filename=""):
-    """This function Prints the contents of a UTF8
-    text file"""
+    """This function Prints the contents of a UTF8 text file"""
     with open(filename, encoding= "utf-8") as mf:
         print(mf.read(), end="")
